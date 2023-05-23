c113bdcacdaa1f41a003013dc29dfe261ac31c33
400kingz <flurpro@gmail.com>
2023-05-23 09:24:52 -0700

    synta1

--- Server/Server.js
+++ Server/Server.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const routes = require('./routes/noteroute')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/noteroute');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // PORT should be in uppercase
 const uri = process.env.MONGODB_URI;
-app.use(express.json())
-app.use(cors())
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
 
-    .catch(err => console.log(err))
+app.use(express.json());
+app.use(cors());
 
-app.use(routes)
-app.listen(port, () => console.log(`Listening on port ${port}`))
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to MongoDB');
+
+    app.use(routes);
+
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1); // Exit process with failure
+});
